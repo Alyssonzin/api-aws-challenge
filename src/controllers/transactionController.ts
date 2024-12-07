4960a94890b1269601818e5d850f568002e7cfc8
Alyssonzin <alyssonempire84@gmail.com>
2024-12-07 17:48:30 -0300

    refactor getTables function to return table names from DynamoDB and handle errors

--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -3,11 +3,11 @@ import { DynamoClient } from "../config/Client";
 import { Request, Response } from "express";
 
 export async function getTables(req: Request, res: Response) {
-    res.send('Hello World');
-    // try {
-    //     const data = await DynamoClient.send(new ListTablesCommand({}));
-    //     return res.status(200).json(data.TableNames);
-    // } catch (error) {
-    //     return res.status(500).json(error);
-    // }
+
+    try {
+        const data = await DynamoClient.send(new ListTablesCommand({}));
+        res.json(data);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
